a6578bf89e444e1aae091a7c48f67f2c78b431ab
socialrupt <79800168+socialrupt@users.noreply.github.com>
2021-03-17 19:20:22 +0000

    <feat> Token stored in csv
    The Bot Token is now stored in a CSV file.

--- Group_1/Group1_Modify_Autodrop_List.py
+++ Group_1/Group1_Modify_Autodrop_List.py
@@ -12,7 +12,7 @@ import pandas as pd
 import csv
 import telegram
 
-updater = Updater("1505711355:AAER5a9RirtlftUFmdkMjQ8F0towhw6Sp1g", use_context=True)
+updater = Updater(token= pd.read_csv("Group_1/Group1_Global_Settings.csv")["Settings_Bot_Key"].iloc[0], use_context=True)
 
 
 dispatcher = updater.dispatcher
@@ -84,15 +84,28 @@ def Add_AutoDrop_Limit_in_Group1_AutoDrop_Settings(update: Update, context: Call
         return Waiting_For_UserName_To_Add_in_Group1_AutoDrop_Settings
 
 def Add_AutoDrop_Member_Task_Complete_Message_in_Group1_AutoDrop_Settings(update: Update, context: CallbackContext) -> int:
-    df11 = pd.read_csv(Save_File_in_Group1_AutoDrop_Settings)
-    df11.fillna(0, inplace=True)  # xhanges all the empty cells to 0
-    df11.loc[len(df11) + 1, "AutoDrop_Limit_Per_Day"] = update.message.text
-    df11.to_csv(Save_File_in_Group1_AutoDrop_Settings, index=False)
-    update.message.reply_text(
-        "The username has been added to your AutoDrop list."
-    )
-    return Waiting_For_User_Choice_in_Group1_AutoDrop_Settings
-
+    try:
+        if type(int(update.message.text)) == int:
+            df11 = pd.read_csv(Save_File_in_Group1_AutoDrop_Settings)
+            print(df11)
+            df11.fillna(0, inplace=True)  # xhanges all the empty cells to 0
+            #len(df11) - 1 means it adds it to the last row and don't create a new row (CURRENTLY WORKING)
+            df11.loc[len(df11) - 1, "AutoDrop_Limit_Per_Day"] = update.message.text
+            df11.to_csv(Save_File_in_Group1_AutoDrop_Settings, index=False)
+            update.message.reply_text(
+                "The username has been added to your AutoDrop list."
+            )
+            return Waiting_For_User_Choice_in_Group1_AutoDrop_Settings
+        else:#This don't work but i have it just in case
+            update.message.reply_text(
+                "Please enter the daily limit for user, which needs to be a number."
+            )
+            return Waiting_For_Limit_To_Add_in_Group1_AutoDrop_Settings
+    except ValueError:#If the input isn't a number then this will run.
+        update.message.reply_text(
+            "You need to send a number, Please try again.😅"
+        )
+        return Waiting_For_Limit_To_Add_in_Group1_AutoDrop_Settings
 #---------------------------------------------------------------
 
 
@@ -142,7 +155,7 @@ def See_List_in_Group1_AutoDrop_Settings(update: Update, context: CallbackContex
     import dataframe_image as dfi
     dfi.export(df, 'Photo_Of_Autodrop_List.png')
 
-    Bot_With_Token = telegram.Bot(token='1505711355:AAER5a9RirtlftUFmdkMjQ8F0towhw6Sp1g')
+    Bot_With_Token = telegram.Bot(token= pd.read_csv("Group_1/Group1_Global_Settings.csv")["Settings_Bot_Key"].iloc[0])
 
     Bot_With_Token.send_photo(chat_id = update.message.chat_id, photo = open("Photo_Of_Autodrop_List.png", "rb"))
 
