fc5c4966b5b537c74d21acb4bfa8815a108bfa5b
socialrupt <79800168+socialrupt@users.noreply.github.com>
2021-03-21 14:50:37 +0000

    Removed the print statments.

--- Group_1/Leeching Bot/Warning_System.py
+++ Group_1/Leeching Bot/Warning_System.py
@@ -26,7 +26,7 @@ def Main_Ting():
     df3 = pd.read_csv("../Group1_Settings.csv")
     #---------------------------------------------------------
     if str(df3.loc[0]["Warning_Status"]).upper() == "ON":
-        print("ITSSSS TUrne on")
+
         for Loop_Over_Links_ChatID in reversed(df1["Link_Chat_Id"]): #Loops over the Give file to get the values
             for Loop_Over_UserIDs in reversed(df1["Link_User_Id"]):
                 for Loop_Over_Users_Warning in reversed(df2["Warns"]):
@@ -69,7 +69,7 @@ def Main_Ting():
                             elif Warns_From_File >= int(df3.loc[0]["NumWarns"]): #Attack the code bro
 
                                 if str(df3.loc[0]["Warning_Punishment"]).upper() == "BAN":
-                                    print("Banning you from this group.")
+
                                     updater.bot.kick_chat_member(str(Loop_Over_Links_ChatID), str(Loop_Over_UserIDs))
 
                                     df1 = pd.read_csv("Warning file test.csv")
@@ -77,7 +77,7 @@ def Main_Ting():
                                     df2.to_csv("Warning file test.csv", index=False)
 
                                 elif str(df3.loc[0]["Warning_Punishment"]).upper() == "MUTE":
-                                    print("Muting you from this group.")
+
                                     Leacher_Muted = ChatPermissions(can_send_messages = False, can_send_media_messages = False, can_send_polls = False, can_send_other_messages = False, can_add_web_page_previews = False, can_change_info = False, can_invite_users = False, can_pin_messages = False)
                                     updater.bot.restrict_chat_member(str(Loop_Over_Links_ChatID), str(Loop_Over_UserIDs), permissions= Leacher_Muted)
 
